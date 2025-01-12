77320b1e83ac6f2a2cb7a881194861b86725e2ec
Deep Sharma <deepshar17@gmail.com>
2025-01-12 19:50:54 +0530

    Paypal intrigration

---
+++ frontend/src/components/paypal.jsx
@@ -0,0 +1,40 @@
+// filepath: /d:/swoc/ShopSphere/frontend/src/components/paypal.jsx
+import React, { useRef, useEffect } from "react";
+
+export default function Paypal({value}) {
+  const paypal = useRef();
+
+  useEffect(() => {
+    if (window.paypal) {
+      window.paypal
+        .Buttons({
+          createOrder: (data, actions, err) => {
+            return actions.order.create({
+              intent: "CAPTURE",
+              purchase_units: [
+                {
+                  description: "Cool looking table",
+                  amount: {
+                    currency_code: "USD",
+                    value: value,
+                  },
+                },
+              ],
+            });
+          },
+          onApprove: async (data, actions) => {
+            const order = await actions.order.capture();
+            console.log(order);
+          },
+          onError: (err) => {
+            console.log(err);
+          },
+        })
+        .render(paypal.current);
+    } else {
+      console.error("PayPal SDK not loaded");
+    }
+  }, []);
+
+  return <div ref={paypal}></div>;
+}
