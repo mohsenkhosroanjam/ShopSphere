8bafe2cbd1d2aaea79f2920e893ac8c5507f0dbe
yashpandav <pandavyash076@gmail.com>
2025-01-13 18:41:28 +0530

    Issue Solved

--- frontend/src/pages/redux/constants.js
+++ frontend/src/pages/redux/constants.js
@@ -1,6 +1,6 @@
 
 
-export const BASE_URL = 'http://localhost:5000' || 'http://localhost:5000'; // Fallback for local development
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000'; // Fallback for local development
 
 export const USERS_URL = `${BASE_URL}/api/users`;
 export const CATEGORY_URL = `${BASE_URL}/api/category`;
